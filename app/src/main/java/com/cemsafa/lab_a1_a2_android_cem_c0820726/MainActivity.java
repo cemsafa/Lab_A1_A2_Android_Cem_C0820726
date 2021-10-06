7837ceff6335f2e6b5186a440f879983dae5bdc9
Cem Safa <c0820726@mylambton.ca>
2021-10-06 19:11:52 -0400

    initial commit

---
+++ app/src/main/java/com/cemsafa/lab_a1_a2_android_cem_c0820726/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cemsafa.lab_a1_a2_android_cem_c0820726;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
